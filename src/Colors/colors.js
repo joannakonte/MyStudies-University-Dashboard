909aeb1fac075902980f6ca0a9eedea6b659a5e9
Lydia Kyriakou <80035248+LydiaKyriakou@users.noreply.github.com>
2023-12-10 12:45:30 +0200

    global colors and logos

---
+++ src/Colors/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    home: '#4E90C9',
+    student: '#0D8BD9',
+    professors: '#004E7F',
+    green: '#1AB17A',
+    orange: '#F67E0E',
+    red: '#F42929',
+    grey: '#EAEAEA',
+    black: '#000000',
+    white: '#FFFFFF',
+};
+  
+export default colors;
