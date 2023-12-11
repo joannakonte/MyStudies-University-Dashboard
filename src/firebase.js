85cc609e555b2a9f68143a3e71dc7a782bfd1790
annakosm <annkos02@gmail.com>
2023-12-11 10:28:14 +0200

    Add firebase basic files

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDDbLw-skAj5rqrskbmHXvEM4mhb70pBs",
+  authDomain: "mystudieseamproject.firebaseapp.com",
+  projectId: "mystudieseamproject",
+  storageBucket: "mystudieseamproject.appspot.com",
+  messagingSenderId: "842517219149",
+  appId: "1:842517219149:web:0d488b953b874db070d904"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
