0382d3ba94d6aaa31e2775fdc22b0d8fd948e96b
annakosm <annkos02@gmail.com>
2023-12-26 09:28:55 +0200

    Added sort and search for tables

--- src/Components/DataTable/DataTable.js
+++ src/Components/DataTable/DataTable.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { db } from '../../firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import './DataTable.css';
-import { HiOutlineEye } from 'react-icons/hi2';
-import PopUp from './PopUp'; 
+import { HiOutlineEye, HiArrowsUpDown, HiMagnifyingGlass } from 'react-icons/hi2';
+import PopUp from './PopUp';
 
 const TableComponent = ({ showOptionColumn }) => {
   const [info, setInfo] = useState([]);
   const [checkboxes, setCheckboxes] = useState({});
   const [selectedClass, setSelectedClass] = useState(null);
+  const [sortColumn, setSortColumn] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetchData();
@@ -40,22 +43,79 @@ const TableComponent = ({ showOptionColumn }) => {
     setSelectedClass(null);
   };
 
+  const toggleSortOrder = (column) => {
+    setSortColumn(column);
+    setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const getSortedData = () => {
+    if (sortColumn) {
+      return [...info].sort((a, b) => {
+        const columnA = a[sortColumn];
+        const columnB = b[sortColumn];
+
+        if (typeof columnA === 'string') {
+          return sortOrder === 'asc' ? columnA.localeCompare(columnB) : columnB.localeCompare(columnA);
+        } else {
+          return sortOrder === 'asc' ? columnA - columnB : columnB - columnA;
+        }
+      });
+    } else {
+      return info;
+    }
+  };
+
+  const filterAndSortData = () => {
+    return getSortedData().filter((classes) => {
+      const { name, ECTS, semester, category } = classes;
+      const normalizedQuery = searchQuery.toLowerCase();
+      return (
+        name.toLowerCase().includes(normalizedQuery) ||
+        ECTS.toString().includes(normalizedQuery) ||
+        semester.toString().includes(normalizedQuery) ||
+        category.toLowerCase().includes(normalizedQuery)
+      );
+    });
+  };
+
   return (
     <div className="table-container">
+    <div className="search-bar">
+      <div className="search-icon-container">
+        <HiMagnifyingGlass className="search-icon" />
+      </div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Αναζήτησε Μάθημα, Εξάμηνο, Κατηγορία ή ECTS"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
       <table className="table">
         <thead>
           <tr className="table-header">
             {showOptionColumn && <th className="table-cell">Επιλογή</th>}
-            <th className="table-cell">Μαθήμα</th>
-            <th className="table-cell">ECTS</th>
-            <th className="table-cell">Κατηγορία</th>
-            <th className="table-cell">Κωδικός</th>
-            <th className="table-cell">Εξάμηνο</th>
+            <th className="table-cell class" onClick={() => toggleSortOrder('name')}>
+              Μαθήμα <HiArrowsUpDown className="icon" />
+            </th>
+            <th className="table-cell icon" onClick={() => toggleSortOrder('ECTS')}>
+              ECTS <HiArrowsUpDown className="icon" />
+            </th>
+            <th className="table-cell icon" onClick={() => toggleSortOrder('category')}>
+              Κατηγορία <HiArrowsUpDown className="icon" />
+            </th>
+            <th className="table-cell icon" onClick={() => toggleSortOrder('id')}>
+              Κωδικός <HiArrowsUpDown className="icon" />
+            </th>
+            <th className="table-cell icon" onClick={() => toggleSortOrder('semester')}>
+              Εξάμηνο <HiArrowsUpDown className="icon" />
+            </th>
             <th className="table-cell">Λεπτομέρειες</th>
           </tr>
         </thead>
         <tbody>
-          {info.map((classes, index) => (
+          {filterAndSortData().map((classes, index) => (
             <tr key={index} className="table-row">
               {showOptionColumn && (
                 <td className="checkbox">
@@ -66,12 +126,14 @@ const TableComponent = ({ showOptionColumn }) => {
                   />
                 </td>
               )}
-              <td className="table-cell">{classes.name}</td>
+              <td className="table-cell class">{classes.name}</td>
               <td className="table-cell">{classes.ECTS}</td>
               <td className="table-cell">{classes.category}</td>
               <td className="table-cell">{classes.id}</td>
               <td className="table-cell">{classes.semester}</td>
-              <td className="eye" onClick={() => openPopup(classes)}><HiOutlineEye /></td>
+              <td className="eye" onClick={() => openPopup(classes)}>
+                <HiOutlineEye />
+              </td>
             </tr>
           ))}
         </tbody>
