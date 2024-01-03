bb37cdd8e44c19ecad89937759eda05e77750d19
joannakonte <ioanna.kontemeniotou@gmail.com>
2024-01-03 10:16:57 +0200

    Trying to fix FAQs

--- src/Components/Students/FAQ/FAQ.js
+++ src/Components/Students/FAQ/FAQ.js
@@ -38,48 +38,51 @@ function FAQ() {
         <Header />
         <Breadcrumb />
         <Sidebar setSelectedSemester={setSelectedSemester} />
-        <h2>Συχνές Ερωτήσεις</h2>
-        
-        <div className={styles.dropdown}>
-          <h2>Ερώτηση Σχετικά με:</h2>
-          <button onClick={toggleDropdown} className={styles['dropdown-toggle']}>
-            {selectedCategory}
-            <FontAwesomeIcon icon={faAngleDown} className={styles['down-arrow']}/>
-          </button>
-          {dropdownOpen && (
-            <div className={`${styles['dropdown-content']} ${dropdownOpen ? styles.show : ''}`}>
-              {categories.map(category => (
-                <button key={category} onClick={() => {
-                  setSelectedCategory(category);
-                  toggleDropdown(); 
-                }}>                  
-                {category}
-                </button>
-              ))}
-            </div>
-          )}
+
+        <div className={styles.faqContainer}>
+          <h2>Συχνές Ερωτήσεις</h2>
+          
+          <div className={styles.dropdown}>
+            <h2>Ερώτηση Σχετικά με:</h2>
+            <button onClick={toggleDropdown} className={styles['dropdown-toggle']}>
+              {selectedCategory}
+              <FontAwesomeIcon icon={faAngleDown} className={styles['down-arrow']}/>
+            </button>
+            {dropdownOpen && (
+              <div className={`${styles['dropdown-content']} ${dropdownOpen ? styles.show : ''}`}>
+                {categories.map(category => (
+                  <button key={category} onClick={() => {
+                    setSelectedCategory(category);
+                    toggleDropdown(); 
+                  }}>                  
+                  {category}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
+          
+          <div className={dropdownOpen ? styles.withDropdownOpen : ''}>
+            {faqData
+              .filter(faq => selectedCategory === 'Όλες' || faq.category === selectedCategory)
+              .map((faq, index) => (
+                <div key={index} className={styles.faqItem}>
+                  <button
+                      className={`${styles.faqButton} ${activeIndex === index ? styles.faqButtonOpen : ''}`}
+                      onClick={() => toggle(index)}
+                  >
+                      <span className={styles.faqQuestionText}>{faq.question}</span>
+                      <FontAwesomeIcon icon={activeIndex === index ? faMinus : faPlus} />
+                  </button>
+                  <div className={activeIndex === index ? styles.faqAnswerOpen : styles.faqAnswer}>
+                      <span className={styles.faqAnswerText}>{faq.answer}</span>
+                  </div>
+              </div>
+            ))}
+          </div>
         </div>
-        
-        <div className={dropdownOpen ? styles.withDropdownOpen : ''}>
-          {faqData
-            .filter(faq => selectedCategory === 'Όλες' || faq.category === selectedCategory)
-            .map((faq, index) => (
-              <div key={index} className={styles.faqItem}>
-                <button
-                    className={`${styles.faqButton} ${activeIndex === index ? styles.faqButtonOpen : ''}`}
-                    onClick={() => toggle(index)}
-                >
-                    <span className={styles.faqQuestionText}>{faq.question}</span>
-                    <FontAwesomeIcon icon={activeIndex === index ? faMinus : faPlus} />
-                </button>
-                <div className={activeIndex === index ? styles.faqAnswerOpen : styles.faqAnswer}>
-                    <span className={styles.faqAnswerText}>{faq.answer}</span>
-                </div>
-            </div>
-          ))}
-        </div>
-      </div>
+    </div>
     );
 }
   
-  export default FAQ;
+export default FAQ;
