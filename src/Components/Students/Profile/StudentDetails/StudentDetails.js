c343536258fde692957d7559aa0edc219a6d6712
LydiaKyriakou <80035248+LydiaKyriakou@users.noreply.github.com>
2024-01-04 19:13:45 +0200

    StudentDetails css grid

--- src/Components/Students/Profile/StudentDetails/StudentDetails.js
+++ src/Components/Students/Profile/StudentDetails/StudentDetails.js
@@ -1,40 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './StudentDetails.module.css'; 
 import Sidebar from '../../../Sidebar/Sidebar';
 import Header from '../../../Header/Header'
 
-// import SemesterTable from '../Classes/SemesterTable';
-// import TableComponent from '../../DataTable/DataTable';
-// import defaultstyle from '../../DataTable/DefaultTable.module.css';
-
 function StudentDetails(){
-    // const [selectedSemester, setSelectedSemester] = useState(1); 
+    const [setSelectedSemester] = useState(1); 
 
-    return(
-        <div className={styles.grid_container}>
-            <div className={styles.item1}>
-                <Header/>
+    return (
+        <div>
+            <Header />
+            <Sidebar setSelectedSemester={setSelectedSemester} />
+
+            <div className={styles.main}>
+                <div className={styles.grid_container}>
+                    {/* Column 1 */}
+                    <div className={styles.item1}>
+                        column1
+                    </div> 
+
+                    {/* Column 2 */}
+                    <div className={styles.item2}>
+                        column2
+                    </div>  
+                </div>
+
+
             </div>
-            <div className={styles.item2}>
-                <Sidebar />
-                {/* Sidebar */}
-            </div>
-            <div className={styles.item3}>
-                {/* main */}
-                {/* <SemesterTable onSelectSemester={setSelectedSemester} />
-                <TableComponent showOptionColumn={false} selectedSemester={selectedSemester} pageStyle={defaultstyle} /> */}
-            </div>  
         </div>
     );
 }
 
 export default StudentDetails;
-
-// import Sidebar from '../../../Sidebar/Sidebar';
-// import Breadcrumb from '../../../Breadcrumb/Breadcrumb';
-
-// <div>
-// <Sidebar />
-// <Breadcrumb />
-// <h1>Στοιχεία Φοιτητή</h1>
-// </div>
